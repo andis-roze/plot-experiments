1bea8ef3352397aa93dbb3e04d3e4fab6f9dd2bf
Andis Roze <andis.roze@sefinance.lv>
2020-12-30 19:09:57 +0200

    Intitial commit: React project boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const app = document.getElementById("root");
+
+if (app) {
+    ReactDOM.render(
+        (
+            <div />
+        ),
+        app
+    );
+}
---
+++ webpack.config.ts
@@ -0,0 +1,88 @@
+import * as path from "path";
+import * as webpack from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import CircularDependencyPlugin from "circular-dependency-plugin";
+
+interface Env {
+    NODE_ENV: string;
+}
+
+type configFactory = (env: Env) => webpack.Configuration;
+
+const createConfig: configFactory = env => {
+    let environment: string;
+
+    switch (env.NODE_ENV) {
+        case "dev":
+            environment = "development";
+            break;
+        case "test":
+            environment = "test";
+            break;
+        case "prod":
+            environment = "production";
+            break;
+        default:
+            environment = "development";
+    }
+
+    return {
+        entry: {
+            "index": "./src/index.tsx",
+        },
+        devtool: "source-map",
+        mode: env.NODE_ENV === "prod" ? "production" : "development",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: ["ts-loader", "tslint-loader"],
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.(scss)$/,
+                    use: [
+                        { loader: "style-loader" }, 
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: true,
+                                importLoaders: 2,
+                            },
+                        }, 
+                        { loader: "postcss-loader" }, 
+                        { loader: "sass-loader" },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                hash: true,
+                title: "Plot experiments",
+                template: "./src/index.html",
+                filename: "./index.html",
+            }),
+            new CircularDependencyPlugin({
+                exclude: /node_modules/,
+                failOnError: true,
+                allowAsyncCycles: false,
+                cwd: process.cwd(),
+            }),
+            new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: [`dist/${environment}`] }),
+        ],
+        resolve: {
+            extensions: [ ".ts", ".tsx", ".js" ]
+        },
+        output: {
+            filename: "[name].js",
+            path: path.resolve(__dirname, "dist", environment),
+        },
+        devServer: {
+            contentBase: path.join(__dirname, "dist", environment),
+        }
+    };
+};
+
+export default createConfig;
